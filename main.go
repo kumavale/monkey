1100e5c88b00a60986b767def5fb3ea19ec60b91
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-05-04 18:04:49 +0900

    Supported reading from file

--- main.go
+++ main.go
@@ -2,18 +2,59 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
 	"monkey/repl"
 	"os"
 	"os/user"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	if len(os.Args) < 2 {
+		user, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		fmt.Printf("Use exit() to exit\n")
+		repl.Start(os.Stdin, os.Stdout)
+	} else {
+		b, err := ioutil.ReadFile(os.Args[1])
+		if err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		run(string(b))
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	fmt.Printf("Use exit() to exit\n")
-	repl.Start(os.Stdin, os.Stdout)
+}
+
+func run(input string) {
+	env := object.NewEnvironment()
+	macroEnv := object.NewEnvironment()
+
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		for _, msg := range p.Errors() {
+			io.WriteString(os.Stdout, "\t"+msg+"\n")
+		}
+		return
+	}
+
+	evaluator.DefineMacros(program, macroEnv)
+	expanded := evaluator.ExpandMacros(program, macroEnv)
+
+	evaluated := evaluator.Eval(expanded, env)
+	if evaluated != nil {
+		io.WriteString(os.Stdout, evaluated.Inspect())
+		io.WriteString(os.Stdout, "\n")
+	}
 }
